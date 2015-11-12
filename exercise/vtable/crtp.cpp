8a9ba1556b0fc26fd46c3e3239261066d823c972
Donjan Rodic <donjan@dyx.ch>
2015-11-12 18:01:17 +0100

    added doxygen support for exercise/vtables/crtp.cpp
    try by:
      cd exercise/vtables/doc
      doxygen
    and view exercise/vtables/doc/html/index.html in a browser

--- exercise/vtable/crtp.cpp
+++ exercise/vtable/crtp.cpp
@@ -1,32 +1,60 @@
-/*******************************************************************************
- *
- * We are looking at v(ege)tables.
- * Requires -std=c++14.
+/** ****************************************************************************
  * 
- * Programming Techniques for Scientific Simulations II, ETH Zürich, 2015
- * For free use, no rights reserved, no warranty whatsoever.
+ * \file
+ * \brief We are looking at v(ege)tables. 
+ * \author Programming Techniques for Scientific Simulations II,
+ *         ETH Zürich, 2015
+ * \date 2015.11.12
+ * \copyright For free use, no rights reserved, no warranty whatsoever.
+ * \warning Requires -std=c++14.
+ * \bug Use pesticide to protect your vegetables against bugs.
  * 
  ******************************************************************************/
+
+/**
+ * 
+ * \mainpage
+ * CRTP demo with one-dimensional vegetables: Carrot and Cucumber
+ * 
+ * \copydetails crtp.cpp
+ *
+ * ### How to use in the main function
+ *
+ * ~~~{.cpp}
+ *     Carrot c;
+ *     c.grow(0.1_growth_per_month);
+ * ~~~
+ *
+ * Look at the source code in main() for further insight:
+ * \dontinclude ./crtp.cpp
+ * \skip iostream
+ * \until Literal
+ * 
+ */
 
 #include <iostream>
 #include <iomanip>
 
-// Literal converter: days => hours
+/// Literal converter: days => hours
 constexpr unsigned int operator"" _days(const unsigned long long int d) {
     return 24 * d;
 }
-// Literal converter: growth per month => growth per hour
+/// Literal converter: growth per month => growth per hour
 constexpr double operator"" _growth_per_month(const long double d) {
     return d/30/24;
 }
+/// Literal converter: growth per month => growth per hour
 constexpr double operator"" _growth_per_month(const unsigned long long int d) {
     return d/30./24;
 }
 
-// c++11 strongly typed (uint8_t) enum
+/// c++11 strongly typed (uint8_t) enum
 enum class VColor: uint8_t {orange, yellow, green, brown, none};
 
-// print a nice string for the entries of VColor
+/** \brief Print a nice string for the entries of VColor
+ *  \param  color        represented as enumeration
+ *  \return std::string  corresponding to enumeration
+ */
 std::string print_color(VColor const & color) {
     switch(color) {
         case VColor::orange:
@@ -42,17 +70,25 @@ std::string print_color(VColor const & color) {
     }
 }
 
-// Class for common capabilities of one-dimensional vegetables
-// Since this is used in a CRTP context, we don't have to use a vtable (no virtual methods)
+/**
+ * \brief Class for common capabilities of one-dimensional vegetables
+ * \details Since this is used in a CRTP context, we don't have to use
+ *          a vtable (no virtual methods)
+*/
 template <typename D>
 class OneDimVegetable {
     public:
 
-    // We can call a static method we don't "know" yet exists.
+    /** \brief Name getter
+     *  \details We can call a static method we don't "know" yet exists.
+     */
     std::string get_name() {
         return print_color(color_) + " " + D::get_name();
     }
 
+    /** \brief Growth function
+     *  \details This is the reason we are interested in vegetables.
+     */
     void grow(const double & d) {      // default growth per hour
         ++age_;
         // The static cast allows us to bypass the vtable lookup. We still have
@@ -62,24 +98,32 @@ class OneDimVegetable {
         weight_ += d * D::linear_density_;
     }
 
-    inline double get_length() { return length_; }
-    inline double get_weight() { return weight_; }
+    inline double get_length() { return length_; } ///< Length getter
+    inline double get_weight() { return weight_; } ///< Weight getter
 
     protected:
-    uint32_t age_ = 0;                         // hours
-    double length_ = 0;                        // length in cm
-    double weight_ = 0;                        // weight in grams
-    VColor color_ = VColor::none;              // color
+    uint32_t age_ = 0;                         ///< hours
+    double length_ = 0;                        ///< length in cm
+    double weight_ = 0;                        ///< weight in grams
+    VColor color_ = VColor::none;              ///< color
 };
 
-// Class for carrots
+/** \brief Class for carrots
+ *  \details They are tasty, but only if they are neither too young nor too old.
+ *  \warning You have to find the sweet spot.
+ */
 class Carrot : public OneDimVegetable<Carrot> {
     using super = OneDimVegetable<Carrot>;
     public:
+    /// \cond DONT_DOC_ME
     friend super;
+    /// \endcond
+
+    /// Name getter
     static const std::string get_name() {
         return "carrot";
     }
+    /// Sets the color according to the age.
     void expire() {
         if(age_ > expiration_time_) // rotten :-/
             color_ = VColor::brown;
@@ -93,14 +137,22 @@ class Carrot : public OneDimVegetable<Carrot> {
     static constexpr uint32_t expiration_time_ = 100_days;
 };
 
-// Class for cucumbers
+/** \brief Class for cucumbers
+ *  \details Slightly denser than carrots.
+ *  \bug These can grow to lightyears in length! The linear density may not be accurate in that case.
+ */
 class Cucumber : public OneDimVegetable<Cucumber> {
     using super = OneDimVegetable<Cucumber>;
     public:
+    /// \cond DONT_DOC_ME
     friend super;
+    /// \endcond
+
+    /// Name getter
     static const std::string get_name() {
         return "cucumber";
     }
+    /// Sets the color according to the age.
     void expire() {
         if(age_ > expiration_time_) // rotten
             color_ = VColor::brown;
@@ -112,6 +164,8 @@ class Cucumber : public OneDimVegetable<Cucumber> {
     static constexpr uint32_t expiration_time_ = 130_days;
 };
 
+// mmm
+/// main function
 int main() {
 
     int time = 14_days;  // OneDimVegetables grow by the hour, but we use days
